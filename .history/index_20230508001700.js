51d65b326c14717dc4a141fd86cfeecf09836c9b
Sean Kipinä <paintballsean@gmail.com>
2023-05-08 00:18:50 +0300

    Fixed some error in volume class as console.log was not right with an error.

--- .history/index_20230508001700.js
+++ .history/index_20230508001700.js
@@ -240,26 +240,23 @@ console.log(cuboid.surfaceArea()); // 130
 //Using CuboidMakerTwo, take your prototypes from above and refactor into class syntax. Then, create an object called cuboidTwo that uses the new keyword to use our CuboidMakerTwo class.
  
 class CuboidMakerTwo {
-  constructor(props) {
-    this.length = props.length;
-    this.width = props.width;
-    this.height = props.height;
+  constructor({length, width, height}) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
   }
-
-  volume() {
+  
+  get volume() {
     return this.length * this.width * this.height;
   }
-
-  surfaceArea() {
+  
+  get surfaceArea() {
     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
   }
 }
 
-const cuboidTwo = new CuboidMakerTwo({
-  length: 4,
-  width: 5,
-  height: 5
-});
+const cuboidTwo = new CuboidMakerTwo({length: 4, width: 5, height: 5});
+
 
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
